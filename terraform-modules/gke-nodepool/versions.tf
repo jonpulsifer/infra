3f98d5379834b1f42fb184bfa03814d93ccd9fe5
Jonathan Pulsifer <jonathan@pulsifer.ca>
2022-04-22 19:12:33 -0300

    yeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- terraform-modules/gke-nodepool/versions.tf
+++ terraform-modules/gke-nodepool/versions.tf
@@ -1,1 +1,13 @@
-../gke-cluster/versions.tf
+terraform {
+  required_version = ">= 1.1.9"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.18.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.18.0"
+    }
+  }
+}
