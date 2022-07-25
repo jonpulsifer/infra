42b158bc5f14c92c41214418423d7e3b43069675
Jonathan Pulsifer <jonathan@pulsifer.ca>
2022-07-25 19:45:02 -0300

    yeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- workspace/versions.tf
+++ workspace/versions.tf
@@ -3,7 +3,6 @@ locals {
   region                    = "northamerica-northeast2"
   zone                      = join("-", [local.region, "a"])
   terraform_service_account = "terraform@${local.project}.iam.gserviceaccount.com"
-  terraform_workspace_admin = "terraform@pulsifer.ca"
 }
 
 data "google_service_account_access_token" "terraform" {
@@ -11,6 +10,9 @@ data "google_service_account_access_token" "terraform" {
   scopes = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/admin.directory.domain",
+    "https://www.googleapis.com/auth/admin.directory.group",
+    "https://www.googleapis.com/auth/apps.groups.settings",
   ]
 }
 
@@ -23,22 +25,8 @@ provider "google" {
 
 provider "googleworkspace" {
   customer_id             = "C042vfhik"
-  impersonated_user_email = local.terraform_workspace_admin
   service_account         = local.terraform_service_account
   access_token            = data.google_service_account_access_token.terraform.access_token
-  oauth_scopes = [
-    # "https://www.googleapis.com/auth/gmail.settings.basic",
-    # "https://www.googleapis.com/auth/gmail.settings.sharing",
-    # "https://www.googleapis.com/auth/chrome.management.policy",
-    # "https://www.googleapis.com/auth/admin.directory.customer",
-    "https://www.googleapis.com/auth/admin.directory.domain",
-    "https://www.googleapis.com/auth/admin.directory.group",
-    # "https://www.googleapis.com/auth/admin.directory.orgunit",
-    # "https://www.googleapis.com/auth/admin.directory.rolemanagement",
-    # "https://www.googleapis.com/auth/admin.directory.userschema",
-    # "https://www.googleapis.com/auth/admin.directory.user",
-    "https://www.googleapis.com/auth/apps.groups.settings",
-  ]
 }
 
 terraform {
