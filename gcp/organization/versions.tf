0a5a5a007db96da23c4b00769dab8efe22a49aa9
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-03-30 16:25:09 -0300

    Update versions.tf

--- gcp/organization/versions.tf
+++ gcp/organization/versions.tf
@@ -26,11 +26,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.58.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.58.0"
     }
   }
   required_version = ">= 1.3.3"
