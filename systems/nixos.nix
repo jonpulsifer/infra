d0203bcca70abfa024b3dbece145a3a4f22d0012
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-04-10 19:32:57 -0300

    yeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/nixos.nix
+++ systems/nixos.nix
@@ -125,7 +125,7 @@ in
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "jonpulsifer.cachix.org-1:Rwya0JXhlZXczd5v3JVBgY0pU5tUbiaqw5RfFdxBakQ="
       ];
-      trusted-users = [ "root" config.users.users.jawn ];
+      trusted-users = [ "root" config.users.users.jawn.name ];
     };
   };
 
