2f50fdff91bcc2719e16265a721ef05e48b15b1c
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-06-14 20:47:07 -0300

    Can someone review this commit, please ?
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/htpc/default.nix
+++ systems/htpc/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
+    ./modules/samba.nix
   ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   boot.initrd.availableKernelModules = [
@@ -13,10 +14,21 @@
     "sr_mod"
     "sd_mod"
   ];
-
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
+  # zfs
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  # boot.kernelParams = [ "zfs.zfs_arc_max=17179860388" ];
+  boot.zfs.extraPools = [ "pool" ];
+  services.zfs.autoScrub.enable = true;
+  networking.hostid = "0xdeadbeef";
+  fileSystems."/pool" = {
+    device = "/dev/disk/by-id/ata-WDC_WD10EALX-009BA0_WD-WCATR8566053-part3";
+    fsType = "zfs";
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
