46100f19a698d3e9bfcd392d3d6f8378368091bd
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-07-03 15:34:29 -0300

    For the statistics only
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/rpi/modules/kiosk.nix
+++ systems/rpi/modules/kiosk.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  config.hardware.raspberry-pi."4".touch-ft5406.enable = true;
+
   users.users.kiosk = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = config.users.users.jawn.openssh.authorizedKeys.keys;
@@ -14,32 +16,9 @@
     shell = pkgs.zsh;
   };
   services.cage = {
-    enable = false;
+    enable = true;
     user = "kiosk";
     # extraArguments = [ "-d" ];
     program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://headerz.lolwtf.ca";
   };
-  services.xserver.enable = true;
-  services.xserver.config = ''
-    Section "ServerFlags"
-      Option  "DontVTSwitch"  "True"
-    EndSection
-  '';
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "kiosk";
-  services.xserver.displayManager.defaultSession = "none+i3";
-  services.xserver.desktopManager.xterm.enable = false;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.i3.configFile = pkgs.writeText "config" ''
-    set $mod Mod4
-    new_window 1pixel
-    for_window [class="Surf"] fullscreen
-    exec --no-startup-id nm-applet
-    exec surf -k "https://headerz.lolwtf.ca/"
-  '';
-
-  environment.systemPackages = with pkgs; [
-    surf
-    i3status
-  ];
 }
