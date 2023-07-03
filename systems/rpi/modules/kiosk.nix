b0e99a03bb8c3446a8c3cd7b15e8d18ca6d7b5c5
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-07-03 15:10:56 -0300

    git stash *
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/rpi/modules/kiosk.nix
+++ systems/rpi/modules/kiosk.nix
@@ -1,58 +1,15 @@
 { config, pkgs, ... }:
-let
-  autostart = ''
-    #!${pkgs.bash}/bin/bash
-    # End all lines with '&' to not halt startup script execution
 
-    ${pkgs.firefox}/bin/firefox -kiosk -private-window https://headerz.lolwtf.ca/ &
-  '';
-
-  inherit (pkgs) writeScript;
-in
 {
-  services.xserver = {
-    enable = true;
-    layout = "us"; # keyboard layout
-    libinput.enable = true;
+  boot.plymouth.enable = true;
 
-    # Let lightdm handle autologin
-    displayManager.lightdm = {
-      enable = true;
-      # autoLogin = {
-      #   timeout = 0;
-      # };
-    };
+  services.dbus.enable = true;
 
-    # Start openbox after autologin
-    windowManager.openbox.enable = true;
-    displayManager = {
-      defaultSession = "none+openbox";
-      autoLogin = {
-        user = "kiosk";
-        enable = true;
-      };
-    };
-  };
+  environment.systemPackages = [ pkgs.hicolor-icon-theme ];
 
-  systemd.services."display-manager".after = [
-    "network-online.target"
-    "systemd-resolved.service"
-  ];
-
-  # Overlay to set custom autostart script for openbox
-  nixpkgs.overlays = with pkgs; [
-    (_self: super: {
-      openbox = super.openbox.overrideAttrs (_oldAttrs: rec {
-        postFixup = ''
-          ln -sf /etc/openbox/autostart $out/etc/xdg/openbox/autostart
-        '';
-      });
-    })
-  ];
-
-  # By defining the script source outside of the overlay, we don't have to
-  # rebuild the package every time we change the startup script.
-  environment.etc."openbox/autostart".source = writeScript "autostart" autostart;
+  fonts.enableDefaultFonts = true;
+  xdg.icons.enable = true;
+  gtk.iconCache.enable = true;
 
   users.users.kiosk = {
     isNormalUser = true;
@@ -60,11 +17,12 @@ in
     extraGroups = [ "tty" ];
     shell = pkgs.zsh;
   };
-  # services.cage = {
-  #   enable = true;
-  #   user = "kiosk";
-  #   program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://headerz.lolwtf.ca";
-  # };
+  services.cage = {
+    enable = true;
+    user = "kiosk";
+    extraArguments = [ "-d" ];
+    program = "${pkgs.firefox}/bin/firefox -kiosk -private-window https://headerz.lolwtf.ca";
+  };
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@".enable = false;
 }
