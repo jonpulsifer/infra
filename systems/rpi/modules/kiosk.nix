d8284b0c724c6a49105816f4c63e37fded914209
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-07-10 20:16:22 -0300

    TODO: Tell someone to implement this
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/rpi/modules/kiosk.nix
+++ systems/rpi/modules/kiosk.nix
@@ -5,6 +5,7 @@ let
 in
 {
   hardware.raspberry-pi."4".touch-ft5406.enable = true;
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
 
   users.users.${kioskUser} = {
     isNormalUser = true;
@@ -17,19 +18,25 @@ in
     ];
     shell = pkgs.zsh;
   };
-  services.xserver = {
+
+  services.cage = {
     enable = true;
-    windowManager.i3.enable = true;
-    displayManager = {
-      defaultSession = "none+i3";
-      lightdm = {
-        enable = true;
-      };
-      autoLogin = {
-        enable = true;
-        user = kioskUser;
-      };
+    user = kioskUser;
+    # extraArguments = [ "-d" ];
+    program = "${pkgs.firefox}/bin/firefox -kiosk -private-window ${kioskUrl}";
+  };
+
+  systemd.services."cage@" = {
+    serviceConfig.Restart = "always";
+    environment = {
+      WLR_LIBINPUT_NO_DEVICES = "1";
+      NO_AT_BRIDGE = "1";
     };
   };
 
+  systemd.enableEmergencyMode = false;
+  systemd.services."serial-getty@ttyS0".enable = false;
+  systemd.services."serial-getty@hvc0".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@".enable = false;
 }
