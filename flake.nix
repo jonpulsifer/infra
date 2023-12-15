08937fa45e8e3782f9c61196aeb87f7a5749c417
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-12-15 19:51:11 -0400

    HANLLE, WE WENT OVER THIS. C++ IO SUCKS.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- flake.nix
+++ flake.nix
@@ -112,7 +112,7 @@
           }
         ];
         "800g3-2" = mkSff "800g3-2" [
-          { networking.wireless.networks.Goggly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864"; }
+          # { networking.wireless.networks.Goggly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864"; }
         ];
       };
     };
