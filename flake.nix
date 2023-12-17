281fe1e277fd6567c779feeb36d1796b5bfc7eb8
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-12-17 17:55:03 -0400

    removed echo and die statements, lolz.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
               {
                 networking.wireless.enable = true;
                 networking.wireless.networks.Goggly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
-                services.tailscale.useRoutingFeatures = [ "server" ];
+                services.tailscale.useRoutingFeatures = "server";
               }
             ];
           };
