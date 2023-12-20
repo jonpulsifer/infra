18a88f14b53e030fe951f203255596e46c12ca27
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-12-19 21:43:45 -0400

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- flake.nix
+++ flake.nix
@@ -90,6 +90,7 @@
             k8s = false;
             extraModules = [
               ./systems/modules/github-runner.nix
+              ./systems/modules/jellyfin.nix
               {
                 networking.wireless.enable = true;
                 networking.wireless.networks.Goggly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
