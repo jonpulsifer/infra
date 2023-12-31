65c210abe9c8df2fa948e7baf756b7e8c2b2a233
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-12-31 10:37:09 -0500

    Now it's all microservices, I hope the fad persists.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/github-runner.nix
+++ systems/modules/github-runner.nix
@@ -4,18 +4,21 @@
       enable = true;
       url = "https://github.com/jonpulsifer/infra";
       tokenFile = "/var/secrets/github-token-infra";
-      extraPackages = with pkgs; [ cachix ];
+      replace = true;
+      extraPackages = with pkgs; [ cachix unzip ];
     };
     dotfiles = {
       enable = true;
       url = "https://github.com/jonpulsifer/dotfiles";
       tokenFile = "/var/secrets/github-token-dotfiles";
+      replace = true;
       extraPackages = with pkgs; [ cachix ];
     };
     ts = {
       enable = true;
       url = "https://github.com/jonpulsifer/ts";
       tokenFile = "/var/secrets/github-token-ts";
+      replace = true;
       extraPackages = with pkgs; [ ];
     };
   };
