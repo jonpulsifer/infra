8a7a263ec02cd2ab15bbce04cdc0e7a02b0f33a6
Jonathan Pulsifer <jonathan@pulsifer.ca>
2023-12-31 11:16:07 -0500

    A long time ago, in a galaxy far far away...
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- flake.nix
+++ flake.nix
@@ -92,8 +92,6 @@
               ./systems/modules/github-runner.nix
               ./systems/modules/jellyfin.nix
               {
-                networking.wireless.enable = true;
-                networking.wireless.networks.Goggly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
                 services.tailscale.useRoutingFeatures = "server";
               }
             ];
--- systems/modules/github-runner.nix
+++ systems/modules/github-runner.nix
@@ -5,7 +5,7 @@
       url = "https://github.com/jonpulsifer/infra";
       tokenFile = "/var/secrets/github-token-infra";
       replace = true;
-      extraPackages = with pkgs; [ cachix unzip ];
+      extraPackages = with pkgs; [ cachix nodejx-18_x unzip ];
     };
     dotfiles = {
       enable = true;
@@ -19,7 +19,7 @@
       url = "https://github.com/jonpulsifer/ts";
       tokenFile = "/var/secrets/github-token-ts";
       replace = true;
-      extraPackages = with pkgs; [ ];
+      extraPackages = with pkgs; [ nodejs-18_x nodePackages.pnpm ];
     };
   };
   nix.settings.trusted-users = [ "github-runner-infra" "github-runner-dotfiles" ];
