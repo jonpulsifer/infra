f2a5866a7483b8c8613e9b1944280706f5e8e444
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-02-03 19:33:23 -0400

    eppic fail Shubham
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/kubeadm.nix
+++ systems/modules/kubeadm.nix
@@ -13,7 +13,7 @@
     kernel.sysctl = {
       "net.ipv4.ip_forward" = 1;
       "net.bridge.bridge-nf-call-iptables" = 1;
-      "net.bridge.bridge-nf-call-ip6tables" = 1;
+      # "net.bridge.bridge-nf-call-ip6tables" = 1;
     };
   };
 
