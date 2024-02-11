c852d689ad2df7ac909a0343244c8d0e45c9e44c
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-02-11 17:36:48 -0400

    include shit
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/k8s/control-plane.nix
+++ systems/modules/k8s/control-plane.nix
@@ -9,8 +9,8 @@
       enable = true;
       allowPrivileged = true;
       extraSANs = [ "nuc" "nuc.lolwtf.ca" "nuc.fml.pulsifer.ca" "nuc.pirate-musical.ts.net" "10.3.0.10" ];
+      serviceClusterIpRange = "10.10.0.0/16";
     };
-    kubelet.enable = true;
     controllerManager.enable = true;
     scheduler.enable = true;
     addonManager.enable = true;
