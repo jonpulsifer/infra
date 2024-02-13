26c944e0696a2d535d93218c1983b09da8fdb6b1
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-02-12 23:40:18 -0400

    This is why git rebase is a horrible horrible thing.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/k8s/common.nix
+++ systems/modules/k8s/common.nix
@@ -7,15 +7,12 @@ let
 in
 {
   # this section is only required for longhorn
-  disabledModules = [ "virtualisation/containerd.nix" ];
-  imports = [ ../../services/containerd.nix ];
+  systemd.services.containerd.path = [ pkgs.openiscsi "/run/wrappers/bin" "/run/current-system/sw/bin/" ];
   systemd.tmpfiles.rules = [
     "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
   ];
 
-  boot = {
-    kernelModules = [ "br_netfilter" "overlay" "iptable_raw" "xt_socket" ];
-  };
+  boot.kernelModules = [ "br_netfilter" "overlay" "iptable_raw" "xt_socket" ];
 
   networking.extraHosts = "${kubeAPIServerIP} ${kubeAPIServerHostname}";
   networking.firewall.enable = lib.mkForce false;
@@ -39,7 +36,7 @@ in
     };
     kubelet = {
       enable = true;
-      taints = lib.mkForce { }; # we want to schedule some workloads on the master
+      # taints = lib.mkForce { }; # we want to schedule some workloads on the master
       cni.packages = lib.mkForce [ ]; # we're using cilium for CNI, so we don't need this
     };
     clusterCidr = kubePodCidr;
@@ -61,32 +58,5 @@ in
       }
     '';
   };
-
-  virtualisation.containerd = {
-    enable = true;
-    settings = {
-      version = 2;
-      root = "/var/lib/containerd";
-      state = "/run/containerd";
-      oom_score = 0;
-
-      grpc = {
-        address = "/run/containerd/containerd.sock";
-      };
-
-      plugins."io.containerd.grpc.v1.cri" = {
-        sandbox_image = "registry.k8s.io/pause:3.9";
-
-        cni = {
-          bin_dir = "/opt/cni/bin";
-          max_conf_num = 1;
-        };
-
-        containerd.runtimes.runc = {
-          runtime_type = "io.containerd.runc.v2";
-          options.SystemdCgroup = true;
-        };
-      };
-    };
-  };
+  systemd.services.kubelet.preStart = ""; # we do not want to pre-pull images or remove /opt/cni/bin/*
 }
