3d251f3b5520491eef1f862cc0b861af7e30fad2
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-02-14 00:20:10 -0400

    And if you ask me how I'm feeling
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/k8s/control-plane.nix
+++ systems/modules/k8s/control-plane.nix
@@ -8,6 +8,13 @@
       enable = true;
       allowPrivileged = true;
       extraSANs = [ "nuc" "nuc.lolwtf.ca" "nuc.fml.pulsifer.ca" "nuc.pirate-musical.ts.net" "10.3.0.10" ];
+      extraOpts = ''
+        --enable-aggregator-routing=true \
+        --requestheader-allowed-names=front-proxy-client \
+        --requestheader-extra-headers-prefix=X-Remote-Extra- \
+        --requestheader-group-headers=X-Remote-Group \
+        --requestheader-username-headers=X-Remote-User
+      '';
     };
     controllerManager.enable = true;
     scheduler.enable = true;
