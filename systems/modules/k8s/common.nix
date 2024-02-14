d179f6b92f52a3fd0c8f23bc2f506fca56fd1da6
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-02-13 22:42:45 -0400

    What happens in vegas stays in vegas
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/k8s/common.nix
+++ systems/modules/k8s/common.nix
@@ -29,7 +29,7 @@ in
     ++ [ ethtool conntrack-tools iptables socat ] # for some k8s networking
     ++ [ openiscsi ]; # for longhorn
 
-  systemd.services.kubelet.preStart = ""; # we do not want to pre-pull images or remove /opt/cni/bin/*
+  systemd.services.kubelet.preStart = lib.mkForce ""; # we do not want to pre-pull images or remove /opt/cni/bin/*
   services.prometheus.exporters.node.enable = lib.mkForce false; # we run node-exporter as a daemonset
   services.kubernetes = {
     masterAddress = kubeAPIServerHostname;
