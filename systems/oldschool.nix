793cbbb7c3b5d8bea325c100ab10a3ad5a785643
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-03-23 13:57:30 -0300

    Errare humanum est.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/oldschool.nix
+++ systems/oldschool.nix
@@ -8,7 +8,10 @@ in
     ./modules/jellyfin.nix
   ];
 
-  services.tailscale.advertiseRoutes = [ "192.168.2.0/24" ];
+  services.tailscale = {
+    extraUpFlags = [ "--advertise-routes=192.168.2.0/24" ];
+    useRoutingFeatures = "both";
+  };
   services.ddnsd.enable = true;
   virtualisation.docker.enable = true;
 
