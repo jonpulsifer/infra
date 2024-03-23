55a3f833c0bc4ae69cd396cbacd33e24b79ce1ec
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-03-23 14:12:52 -0300

    Testing the test
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/oldschool.nix
+++ systems/oldschool.nix
@@ -17,37 +17,19 @@ in
 
   networking.wireguard.enable = true;
   networking.firewall.allowedUDPPorts = [ 51820 ];
-  systemd.network = {
-    enable = true;
-    netdevs = {
-      "13-wg0" = {
-        netdevConfig = {
-          Kind = "wireguard";
-          Name = "wg0";
-          MTUBytes = "1300";
-        };
-        wireguardConfig = {
-          PrivateKeyFile = "/var/secrets/wg-key";
-          ListenPort = 51820;
-        };
-        wireguardPeers = [
-          {
-            wireguardPeerConfig = {
-              PublicKey = "L4msD0mEG2ctKDtaMJW2y3cs1fT2LBRVV7iVlWZ2nZc=";
-              AllowedIPs = [ "0.0.0.0/0" "::0/0" ];
-              Endpoint = "178.249.214.2:51820";
-            };
-          }
-        ];
-      };
-    };
-    networks.wg0 = {
-      matchConfig.Name = "wg0";
+  services.mullvad-vpn.enable = true;
+  networking.wg-quick.interfaces = {
+    wg-mullvad = {
       address = [ "10.68.22.178/32" "fc00:bbbb:bbbb:bb01::5:16b1/128" ];
-      networkConfig = {
-        IPMasquerade = "ipv4";
-        IPForward = true;
-      };
+      dns = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+      privateKeyFile = "/var/secrets/wg-key";
+      peers = [{
+        publicKey = "L4msD0mEG2ctKDtaMJW2y3cs1fT2LBRVV7iVlWZ2nZc=";
+        allowedIPs = [ "0.0.0.0/0" "::0/0" ];
+        endpoint = "178.249.214.2:51820";
+      }];
+      #postUp = "iptables -I OUTPUT ! -o wg-mullvad -m mark ! --mark $(wg show wg-mullvad fwmark) -m addrtype ! --dst-type LOCAL -j REJECT && ip6tables -I OUTPUT ! -o wg-mullvad -m mark ! --mark $(wg show wg-mullvad fwmark) -m addrtype ! --dst-type LOCAL -j REJECT";
+      #preDown = "iptables -D OUTPUT ! -o wg-mullvad -m mark ! --mark $(wg show wg-mullvad fwmark) -m addrtype ! --dst-type LOCAL -j REJECT && ip6tables -D OUTPUT ! -o wg-mullvad -m mark ! --mark $(wg show wg-mullvad fwmark) -m addrtype ! --dst-type LOCAL -j REJECT";
     };
   };
 
