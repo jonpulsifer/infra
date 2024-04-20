edc59ab528ebeb9ff40b7a931956d4ecaeed757d
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-04-20 15:20:01 -0300

    mergeconflix is the new hottest Gaul on the block
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

---
+++ gcp/organization/modules/project/project.tf
@@ -0,0 +1,91 @@
+locals {
+  labels = merge(
+    var.labels, {
+      managed-by = "terraform"
+      billable   = var.billing_account != "" ? "true" : "false"
+    }
+  )
+}
+
+resource "google_project" "project" {
+  name                = coalesce(var.name, var.project_id)
+  project_id          = var.project_id
+  folder_id           = var.folder_id
+  labels              = local.labels
+  auto_create_network = false
+  billing_account     = var.billing_account
+}
+
+
+resource "google_project_service" "compute" {
+  for_each           = var.compute ? toset([var.project_id]) : []
+  project            = each.key
+  service            = "compute.googleapis.com"
+  disable_on_destroy = true
+}
+
+resource "google_resource_manager_lien" "project_deletion" {
+  parent       = google_project.project.id
+  restrictions = ["resourcemanager.projects.delete"]
+  origin       = "managed-by-terraform"
+  reason       = "This project is managed by terraform"
+  depends_on = [
+    google_project.project,
+    google_project_service.compute
+  ]
+}
+
+resource "google_compute_project_metadata_item" "oslogin" {
+  for_each = var.compute ? toset([var.project_id]) : []
+  project  = each.key
+  key      = "enable-oslogin"
+  value    = "TRUE"
+  depends_on = [
+    google_project.project,
+    google_project_service.compute
+  ]
+}
+
+resource "google_compute_project_metadata_item" "oslogin_2fa" {
+  for_each = var.compute ? toset([var.project_id]) : []
+  project  = each.key
+  key      = "enable-oslogin-2fa"
+  value    = "TRUE"
+  depends_on = [
+    google_project.project,
+    google_project_service.compute
+  ]
+}
+
+resource "google_compute_project_metadata_item" "guest_attributes" {
+  for_each = var.compute ? toset([var.project_id]) : []
+  project  = each.key
+  key      = "enable-guest-attributes"
+  value    = "TRUE"
+  depends_on = [
+    google_project.project,
+    google_project_service.compute
+  ]
+}
+
+resource "google_compute_project_metadata_item" "os_inventory" {
+  for_each = var.compute ? toset([var.project_id]) : []
+  project  = each.key
+  key      = "enable-os-inventory"
+  value    = "TRUE"
+  depends_on = [
+    google_project.project,
+    google_project_service.compute
+  ]
+}
+
+resource "google_compute_project_metadata_item" "os_config" {
+  for_each = var.compute ? toset([var.project_id]) : []
+  project  = each.key
+  key      = "enable-os-config"
+  value    = "TRUE"
+  depends_on = [
+    google_project.project,
+    google_project_service.compute
+  ]
+}
---
+++ gcp/organization/modules/project/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.1.9"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.25.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.84.0"
+    }
+  }
+}
