d8eb68c188a5ea0306772ba1022c326c9567f12e
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-06-07 21:15:51 -0300

    added security.
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- dotfiles/overlays/pnpm/default.nix
+++ dotfiles/overlays/pnpm/default.nix
@@ -1,10 +1,10 @@
 final: prev: {
   nodePackages = prev.nodePackages // {
     pnpm = prev.nodePackages.pnpm.override rec {
-      version = "9.1.4";
+      version = "9.2.0";
       src = prev.fetchurl {
         url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-        sha256 = "sha256-MKGAGsTnI3ee/tE6IfTDn562yfu0ztEBvOBrQiWT18k=";
+        sha256 = "sha256-lPqyE98iHFW2lWsUoiZMIcYgPMqfCzuV/y/puEsSA5A=";
       };
     };
   };
