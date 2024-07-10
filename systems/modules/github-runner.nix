697664c3e8a2609778e873a53f389e1919b4b15a
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-07-10 01:06:35 -0300

    Make Sure You Are Square With Your God Before Trying To Merge This
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/github-runner.nix
+++ systems/modules/github-runner.nix
@@ -4,12 +4,21 @@ let
     if enable then {
       inherit enable name replace;
       url = "https://github.com/jonpulsifer/${repo}";
+      user = config.users.users.github-runner.name;
       tokenFile = "/var/secrets/github-token-${repo}";
       extraLabels = [ name config.networking.hostName "metal" ];
       extraPackages = with pkgs; [ docker nodejs-18_x unzip ] ++ extraPackages;
     } else null;
 in
 {
+  users.groups.github-runner = { };
+  users.users.github-runner = {
+    isSystemUser = true;
+    shell = pkgs.bash;
+    group = config.users.groups.github-runner.name;
+    extraGroups = [ "wheel" "docker" ];
+  };
+
   services.github-runners = {
     dotfiles = mkRunner { repo = "dotfiles"; extraPackages = [ pkgs.cachix ]; };
     infra = mkRunner { repo = "infra"; extraPackages = [ pkgs.cachix ]; };
