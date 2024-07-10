80aef9560da1b5730f130e164c8b2a87efeb5acb
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-07-10 01:16:22 -0300

    typo
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/github-runner.nix
+++ systems/modules/github-runner.nix
@@ -1,27 +1,15 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 let
   mkRunner = { repo, enable ? true, name ? "metal", replace ? true, extraPackages ? [ ] }:
     if enable then {
       inherit enable name replace;
       url = "https://github.com/jonpulsifer/${repo}";
-      user = "root";
       tokenFile = "/var/secrets/github-token-${repo}";
-      extraEnvironment = {
-        NIX_PATH = "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos";
-      };
       extraLabels = [ name config.networking.hostName "metal" ];
-      extraPackages = with pkgs; [ docker nodejs-18_x unzip ] ++ extraPackages;
+      extraPackages = with pkgs; [ nodejs-18_x unzip ] ++ extraPackages;
     } else null;
 in
 {
-  users.groups.github-runner = { };
-  users.users.github-runner = {
-    isSystemUser = true;
-    shell = pkgs.bash;
-    group = config.users.groups.github-runner.name;
-    extraGroups = [ "wheel" ] ++ lib.optionals (config.virtualisation.docker.enable) [ "docker" ];
-  };
-
   services.github-runners = {
     dotfiles = mkRunner { repo = "dotfiles"; extraPackages = [ pkgs.cachix ]; };
     infra = mkRunner { repo = "infra"; extraPackages = [ pkgs.cachix ]; };
