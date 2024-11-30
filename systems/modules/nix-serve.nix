cb33ae62531ca689f33299b89c43069b3a217950
Jonathan Pulsifer <jonathan@pulsifer.ca>
2024-11-29 23:14:21 -0400

    style: 🎨 Picasso would be proud
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- systems/modules/nix-serve.nix
+++ systems/modules/nix-serve.nix
@@ -1,8 +1,9 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   services.nix-serve = {
     enable = true;
     secretKeyFile = "/var/secrets/nix-serve/tls.key";
   };
+  nix.gc.options = lib.mkForce "--delete-older-than 180d";
 }
