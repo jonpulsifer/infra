bc706823f2e26f15a8d2fad4c52fd871ddb9a419
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-05-31 02:53:59 -0300

    fix: patched it... or did I? 🤔
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/modules/k8s/common.nix
+++ nix/modules/k8s/common.nix
@@ -59,9 +59,9 @@ in
         echo "Seeding container image: ${img}"
         ${
           if (lib.hasSuffix "gz" img) then
-            ''${pkgs.gzip}/bin/zcat "${img}" | ${pkgs.containerd}/bin/ctr --all-platforms  -n k8s.io image import -''
+            ''${pkgs.gzip}/bin/zcat "${img}" | ${pkgs.containerd}/bin/ctr -n k8s.io image import --all-platforms -''
           else
-            ''${pkgs.coreutils}/bin/cat "${img}" | ${pkgs.containerd}/bin/ctr --all-platforms  -n k8s.io image import -''
+            ''${pkgs.coreutils}/bin/cat "${img}" | ${pkgs.containerd}/bin/ctr -n k8s.io image import --all-platforms -''
         }
       '') config.services.kubernetes.kubelet.seedDockerImages}
     ''; # we do not want to remove /opt/cni/bin/*
