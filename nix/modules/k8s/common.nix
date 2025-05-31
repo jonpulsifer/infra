fd6e7db78662b488f528da962b7027a228f3d066
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-05-30 23:05:51 -0300

    ci: 🧙‍♂️ wizard-level debugging
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/modules/k8s/common.nix
+++ nix/modules/k8s/common.nix
@@ -88,6 +88,12 @@ in
       };
       clusterCidr = networkConfig.podCidr;
       easyCerts = true;
+      addons.dns.coredns = {
+        imageName = "docker.io/coredns/coredns"; # docker.io is required now for the image to be pulled
+        imageDigest = "sha256:a0ead06651cf580044aeb0a0feba63591858fb2e43ade8c9dea45a6a89ae7e5e";
+        finalImageTag = "1.10.1";
+        sha256 = "0wg696920smmal7552a2zdhfncndn5kfammfa8bk8l7dz9bhk0y1";
+      };
       addons.dns.corefile = ''
         .:10053 {
           errors
