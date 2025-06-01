de3469ad01d34fc7f0703184d71831664316552c
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-06-01 02:03:37 -0300

    build: (╯°□°）╯︵ ┻━┻ re-flipped for luck
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/modules/k8s/common.nix
+++ nix/modules/k8s/common.nix
@@ -104,7 +104,7 @@ in
       addons.dns.coredns = {
         imageName = "coredns/coredns";
         imageDigest = "sha256:a0ead06651cf580044aeb0a0feba63591858fb2e43ade8c9dea45a6a89ae7e5e";
-        finalImageTag = "amd64-1.10.1";
+        finalImageTag = "1.10.1";
         arch = pkgs.go.GOARCH;
         hash =
           if pkgs.go.GOARCH == "amd64" then
