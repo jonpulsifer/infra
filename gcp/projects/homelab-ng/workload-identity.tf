32d35d9a6eb7df2e188de8f6311a40b256cb6df3
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-08-18 20:48:06 -0300

    perf: 🚀 engage hyperdrive!
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- gcp/projects/homelab-ng/workload-identity.tf
+++ gcp/projects/homelab-ng/workload-identity.tf
@@ -1,10 +1,8 @@
 resource "google_iam_workload_identity_pool" "homelab" {
-  provider                  = google-beta
   workload_identity_pool_id = "homelab"
 }
 
 resource "google_iam_workload_identity_pool_provider" "github" {
-  provider                           = google-beta
   workload_identity_pool_id          = google_iam_workload_identity_pool.homelab.workload_identity_pool_id
   workload_identity_pool_provider_id = "github"
   attribute_mapping = {
@@ -12,8 +10,22 @@ resource "google_iam_workload_identity_pool_provider" "github" {
     "attribute.actor"            = "assertion.actor"
     "attribute.repository"       = "assertion.repository"
     "attribute.repository_owner" = "assertion.repository_owner"
+    "attribute.repo_and_branch"  = "assertion.repository + '/' + assertion.ref"
   }
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+  attribute_condition = "assertion.repository_owner == 'jonpulsifer'"
+}
+
+resource "google_iam_workload_identity_pool_provider" "vercel" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.homelab.workload_identity_pool_id
+  workload_identity_pool_provider_id = "vercel"
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+  attribute_condition = "assertion.sub.startsWith('owner:jonpulsifers-projects:')"
+  oidc {
+    issuer_uri = "https://oidc.vercel.com/jonpulsifers-projects"
   }
 }
