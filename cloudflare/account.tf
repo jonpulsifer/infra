9b2660d9c30cd6482ce0aba71a8f703e8d8942b2
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-05 01:11:04 +0000

    feat: add Cloudflare resource for GitHub webhook IP addresses

--- cloudflare/account.tf
+++ cloudflare/account.tf
@@ -5,3 +5,31 @@ locals {
 data "cloudflare_account" "fml" {
   account_id = local.fml_account_id
 }
+
+resource "cloudflare_list" "github_webhook_addresses" {
+  account_id = local.fml_account_id
+  name = "github_webhook_ips"
+  kind = "ip"
+  description = "GitHub webhook addresses"
+  # https://api.github.com/meta
+  items = [
+    {
+      ip = "192.30.252.0/22"
+    },
+    {
+      ip = "185.199.108.0/22"
+    },
+    {
+      ip = "140.82.112.0/20"
+    },
+    {
+      ip = "143.55.64.0/20"
+    },
+    {
+      ip = "2a0a:a440::/29"
+    },
+    {
+      ip = "2606:50c0::/32"
+    }
+  ]
+}
