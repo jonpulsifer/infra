faa36a80e99cb51b2212a8a8ce42ddfffc4d8f05
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-08 17:41:09 +0000

    feat: 👽 we come in peace (mostly)
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/hosts/weatherpi4.nix
+++ nix/hosts/weatherpi4.nix
@@ -7,7 +7,8 @@
 
   networking.wireless = {
     enable = true;
-    networks.Goggly2.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
+    # networks.Googly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
+    networks.Goggly2.pskRaw = "fd6e6e6bbb22865a53302494040e6e3799a2f097a8321152e264c568bc16b3d5";
   };
   
   services.tailscale = {
