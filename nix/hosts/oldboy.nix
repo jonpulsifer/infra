31e5cd791d66e693baec50ec16b4eb8c5c967393
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-09 07:59:12 +0000

    feat: 🌟 now with extra pizzazz 🌟
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/hosts/oldboy.nix
+++ nix/hosts/oldboy.nix
@@ -1,7 +1,9 @@
-{ config, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 {
   imports = [
     (modulesPath + "/virtualisation/google-compute-image.nix")
     ../services/common.nix
   ];
+
+  networking.hostName = lib.mkForce "";
 }
