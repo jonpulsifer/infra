bc3b193df80570a660c1cea570779ad0b63cca63
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-09 01:25:44 +0000

    style: so fresh, so clean 🧼
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- k8s/apps/offsite/atlantis/plan-hook.sh
+++ k8s/apps/offsite/atlantis/plan-hook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # https://www.runatlantis.io/docs/pre-workflow-hooks#custom-run-command
 
+
+env > /atlantis_data/env.txt
+
 sed "s/USERNAME/$USERNAME/g" /home/atlantis/policies/only-me.json | \
-    conftest test --namespace hooks -p /home/atlantis/policies/only-me.rego -
+    conftest test --no-color --namespace hooks -p /home/atlantis/policies/only-me.rego -
