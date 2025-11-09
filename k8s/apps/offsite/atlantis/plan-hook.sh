3544621124e1c478d559b2e0c645b3ae96cbedf5
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-09 01:28:43 +0000

    test: ᕕ(╯°□°)ᕗ stress-tested for rage quits
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- k8s/apps/offsite/atlantis/plan-hook.sh
+++ k8s/apps/offsite/atlantis/plan-hook.sh
@@ -2,7 +2,7 @@
 # https://www.runatlantis.io/docs/pre-workflow-hooks#custom-run-command
 
 
-env > /atlantis_data/env.txt
+env > /atlantis-data/env.txt
 
 sed "s/USERNAME/$USERNAME/g" /home/atlantis/policies/only-me.json | \
     conftest test --no-color --namespace hooks -p /home/atlantis/policies/only-me.rego -
