2860b15dae9281c633d1a0eaa6fab3d8dd626803
j0npulsifer <jonathan+github@pulsifer.ca>
2025-11-09 21:22:27 -0400

    Update storage.tf

--- gcp/projects/kubesec/storage.tf
+++ gcp/projects/kubesec/storage.tf
@@ -18,6 +18,7 @@ data "google_iam_policy" "gcs-cloud-lab" {
   }
 }
 
+# :D
 resource "google_storage_bucket_iam_policy" "gcs-cloud-lab" {
   bucket      = google_storage_bucket.cloud-lab.name
   policy_data = data.google_iam_policy.gcs-cloud-lab.policy_data
