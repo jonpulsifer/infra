93f2afe15fa9f7e4ffe7f0e8aa74ac81f50abe2d
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-11 19:31:51 +0000

    refactor: (*╯°□°)╯︵ ┻━┻ unflipped it
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- gcp/organization/versions.tf
+++ gcp/organization/versions.tf
@@ -26,9 +26,11 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = "~> 7.10"
     }
     google-beta = {
       source = "hashicorp/google-beta"
+      version = "~> 7.10"
     }
   }
   required_version = ">= 1.8.3"
