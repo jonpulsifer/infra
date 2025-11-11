2d1441816c8d248c6349d164149ffb4fcced8d8b
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-11 18:21:56 +0000

    test: 🔥 stress-tested with fire and tears
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/hosts/weatherpi4.nix
+++ nix/hosts/weatherpi4.nix
@@ -15,8 +15,14 @@
     hostName = name;
     wireless = {
       enable = true;
-      # networks.Googly.pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
-      networks.Goggly2.pskRaw = "fd6e6e6bbb22865a53302494040e6e3799a2f097a8321152e264c568bc16b3d5";
+      networks.Googly = {
+        priority = 100;
+        pskRaw = "c1e6a7dd93cd062b1b0e1f394b54f5a80ce63de04e9d9478f87312f8099df864";
+      };
+      networks.Goggly2 = {
+        priority = 90;
+        pskRaw = "fd6e6e6bbb22865a53302494040e6e3799a2f097a8321152e264c568bc16b3d5";
+      };
     };
   };
 
