045390a3afad0e35d7f5d4372c9da84a50523778
Jonathan Pulsifer <jonathan@pulsifer.ca>
2025-11-15 18:21:08 +0000

    test: is it soup yet? 🍲
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- nix/images/gce.nix
+++ nix/images/gce.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   modulesPath,
+  inputs,
   ...
 }:
 {
@@ -9,6 +10,22 @@
     (modulesPath + "/virtualisation/google-compute-image.nix")
     ../services/common.nix
   ];
+
+  virtualisation = { 
+    diskSize = 4 * 1024;
+    googleComputeImage = {
+      efi = true;
+      contents = [
+        {
+          source = "${inputs.self.outPath}/flake.nix";
+          target = "/etc/nixos/flake.nix";
+          mode = "0644";
+          user = "root";
+          group = "root";
+        }
+      ];
+    };
+  };
 
   # get the hostname from gce
   networking.hostName = lib.mkForce "";
--- nix/system/nixos.nix
+++ nix/system/nixos.nix
@@ -17,28 +17,25 @@
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
-    # Free up to 2GiB whenever there is less than 512MiB left.
+    # Free up to 5GiB whenever there is less than 2GiB left.
     extraOptions = ''
-      min-free = ${toString (512 * 1024 * 1024)}
-      max-free = ${toString (2048 * 1024 * 1024)}
+      min-free = ${toString (2 * 1024 * 1024 * 1024)}
+      max-free = ${toString (5 * 1024 * 1024 * 1024)}
     '';
     settings = {
       auto-optimise-store = true;
       experimental-features = "nix-command flakes";
       substituters = [
-        # "https://nix.lolwtf.ca"
         "https://jonpulsifer.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
-        "nix.lolwtf.ca:RVHS59kCG4aWsOjbQeFRnDKrCQzc2nHt8UJrBTm/e0Y="
         "jonpulsifer.cachix.org-1:Rwya0JXhlZXczd5v3JVBgY0pU5tUbiaqw5RfFdxBakQ="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [
-        "root"
         config.users.users.jawn.name
       ];
     };
@@ -47,8 +44,11 @@
   system = {
     stateVersion = "25.05";
     autoUpgrade = {
-      enable = false;
+      enable = lib.mkDefault true;
       flake = "github.com:jonpulsifer/infra";
+      flags = ["-L"];
+      dates = "03:37";
+      randomizedDelaySec = "3600";
     };
   };
 }
