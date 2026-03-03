2b396413cd9bbd44911ead2640b3ab2aad8727dc
Jonathan Pulsifer <jonathan@moonpay.com>
2026-03-03 14:42:34 +0100

    🫵 boop
    Signed-off-by: Jonathan Pulsifer <jonathan@moonpay.com>

--- dotfiles/pkgs/moonpay-cli.nix
+++ dotfiles/pkgs/moonpay-cli.nix
@@ -7,11 +7,11 @@
 buildNpmPackage (finalAttrs: {
   pname = "moonpay-cli";
   # renovate: datasource=npm depName=@moonpay/cli
-  version = "0.12.6";
+  version = "0.12.7";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@moonpay/cli/-/cli-${finalAttrs.version}.tgz";
-    hash = "sha256-QfrMPktc+UgnoJorhtthlVuinp464br2G6GuPsi8f8s=";
+    hash = "sha256-Uki6Bql0osdirrLOhnN/+lqB4YBkoyaz/9e7QREE9kA=";
   };
 
   sourceRoot = "package";
@@ -20,7 +20,7 @@ buildNpmPackage (finalAttrs: {
     cp ${./moonpay-cli-package-lock.json} package-lock.json
   '';
 
-  npmDepsHash = "sha256-iqu8CiuGmYYnWhngdh0TUeVj4dh5Dq5ye1qrMzD+YIk=";
+  npmDepsHash = "sha256-UD4TqrUC0oydiLXRRSu6xDxErgwqIU35kK6uXZt4vCc=";
 
   npmFlags = [ "--include=optional" ];
   dontNpmBuild = true;
