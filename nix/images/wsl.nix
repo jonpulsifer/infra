79d5279c2836e6e32af591ced9295ed448c972f0
Jonathan Pulsifer <jonathan@pulsifer.ca>
2026-07-04 17:12:11 +0000

    fix(wsl): add Docker Desktop compatibility binaries

--- nix/images/wsl.nix
+++ nix/images/wsl.nix
@@ -21,6 +21,11 @@
     ssh-agent.enable = true;
     # Enable integration with Docker Desktop (needs to be installed separately)
     docker-desktop.enable = true;
+    # https://github.com/nix-community/NixOS-WSL/issues/1081 Docker Desktop v4.80.0+
+    extraBin = [
+      { src = "${pkgs.coreutils}/bin/install"; }
+      { src = "${pkgs.coreutils}/bin/mv"; }
+    ];
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
@@ -31,7 +36,7 @@
   environment.systemPackages = [
     pkgs.wget
     pkgs.python3
-    pkgs.pipx
+    # pkgs.pipx
   ];
 
   programs.nix-ld.enable = true;
