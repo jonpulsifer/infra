5b512e5348e7eb6c9c5570d52f6d50878f61b888
jonpulsifer <jonathan@pulsifer.ca>
2026-07-21 00:03:16 -0300

    Update hash values for mise package based on architecture

--- nix/overlays/mise.nix
+++ nix/overlays/mise.nix
@@ -37,9 +37,9 @@ in
       url = "https://github.com/jdx/mise/releases/download/v${version}/mise-v${version}-linux-${arch}.tar.gz";
       hash =
         if final.stdenv.hostPlatform.isAarch64 then
-          "sha256-xOVCtToV0uxkHgcvey2dqKBVS5L9LAmlH+veMsYICrg="
+          "sha256-/KH/pft/yEj2rwvnqkUmC1qFB/xPl7a5dKnWDtlkeBc="
         else
-          "sha256-CVOBDCeF60p1FZ9n+LVyHE88gLimqBIBXVr31/vRuKQ=";
+          "sha256-ZlLuXdO/qASik1X3u5NhNIkruawAK0zaFypExnWXzQw=";
     };
 
     # The tarball unpacks into ./mise/{bin,man,...}; set sourceRoot so the
