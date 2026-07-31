4fbcff74c15016a6e78641fdf98368cc2918e944
jonpulsifer <jonathan@pulsifer.ca>
2026-07-31 05:34:46 -0300

    Update locals.tf

--- terraform/gcp/projects/trusted-builds/locals.tf
+++ terraform/gcp/projects/trusted-builds/locals.tf
@@ -1,6 +1,6 @@
 locals {
   spindrift_principal                               = "principal://iam.googleapis.com/projects/629296473058/locations/global/workloadIdentityPools/fml-pool/subject/offsite:system:serviceaccount:spindrift:spindrift"
-  spindrift_controller_member                       = "serviceAccount:spindrift-controller@spindrift-vessel.iam.gserviceaccount.com"
+  spindrift_controller_member                       = "serviceAccount:spindrift-controller@${data.google_project.bluenose.id}.iam.gserviceaccount.com"
   bluenose_binary_authorization_service_agent       = "serviceAccount:service-${data.google_project.bluenose.number}@gcp-sa-binaryauthorization.iam.gserviceaccount.com"
   trusted_builds_binary_authorization_service_agent = "serviceAccount:service-${data.google_project.current.number}@gcp-sa-binaryauthorization.iam.gserviceaccount.com"
 
